1941a1a99a909cb1a6065002faa6e6204a7b5f44
rkdrud34 <rkdrud34@nate.com>
2022-02-14 20:38:47 +0900

    feat: 권한별 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { CountPage } from './pages';
+import { CountPage, AuthPage } from './pages';
+import { AuthRoute, PublicRoute } from './components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from 'styles/global-styles';
 
@@ -8,6 +9,9 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<CountPage />} />
+
+        <Route path="/private" element={<AuthRoute element={AuthPage} />} />
+        <Route path="/pulbic" element={<AuthRoute element={CountPage} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
 export { default as CountPage } from './CountPage';
+export { default as AuthPage } from './AuthPage';
