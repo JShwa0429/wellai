b7c7fe48f9e6f726a4f0b9ce21b56d8d032bf9b7
Martina Lee <martinalee0528@gmail.com>
2022-03-08 00:02:48 +0900

    feat: 메인페이지 레이아웃 수정

--- src/pages/MainRenderPage.tsx
+++ src/pages/MainRenderPage.tsx
@@ -24,9 +24,9 @@ const MainRenderPageTemp = () => {
       </SectionOne>
       <SectionTwo>
         <div className="introduce">
-          <h1>당신의 퍼스널 AI 트레이너</h1>
+          <h1>당신의 퍼스널 AI트레이너</h1>
           <p>
-            AI 트레이너의 실시간 운동자세 피드백,
+            AI트레이너의 실시간 운동자세 피드백,
             <br />
             혼자서 하는 운동도 이젠 두렵지 않아!
           </p>
@@ -37,17 +37,22 @@ const MainRenderPageTemp = () => {
       </SectionTwo>
       <SectionThree>
         <div className="introduce">
+          <div className="contents">
+            <h1>
+              한 눈에 보는
+              <br />
+              스마트한 건강 관리
+            </h1>
+            <p>
+              당신의 노력을 한 눈에 보기 쉽게.
+              <br />
+              마이 리포트로 주간/월간 운동 기록을 체크해보세요.
+            </p>
+          </div>
+          {/* <div className="circle">
+            <img src="image/ellipse.png" alt="원" />
+          </div> */}
           <img src="image/ellipse.png" alt="원" />
-          <h1>
-            한 눈에 보는
-            <br />
-            스마트한 건강 관리
-          </h1>
-          <p>
-            마이 리포트로 주간/월간 운동 기록을 체크해보세요.
-            <br />
-            당신의 노력을 한 눈에 보기 쉽게!
-          </p>
         </div>
         <div className="image">
           <div>
@@ -88,8 +93,8 @@ const SectionOne = styled.section`
   margin-top: -80px;
 
   h1 {
-    font-size: 3.5em;
-    font-weight: 900;
+    font-size: 65px;
+    font-weight: 800;
     line-height: 110%;
     background: linear-gradient(to right, #ffafbd, #ffc3a0);
     color: transparent;
@@ -101,7 +106,8 @@ const SectionOne = styled.section`
 
   p {
     font-family: 'Noto Sans KR';
-    font-weight: 400;
+    font-weight: 100;
+    font-size: 20px;
     margin-right: auto;
   }
 
@@ -119,19 +125,24 @@ const SectionOne = styled.section`
   }
 
   .introduce {
-    font-size: 1.5em;
+    font-size: 1.4em;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin-left: auto;
+    margin-left: 160px;
+    letter-spacing: -0.5px;
+    h1 {
+      letter-spacing: -2px;
+    }
   }
 
   .image {
-    max-width: 700px;
-    margin: auto;
+    width: 700px;
+    margin: -80px 0 80px auto;
     object-fit: cover;
     img {
+      min-width: 500px;
       height: 100vh;
       min-height: 600px;
     }
@@ -152,15 +163,17 @@ const SectionTwo = styled.section`
     border-bottom: 1px solid black;
     text-align: center;
     h1 {
-      font-size: 3em;
-      font-weight: 900;
+      font-size: 65px;
+      font-weight: 800;
       margin: 0;
+      letter-spacing: -2px;
     }
 
     p {
       font-color: ${(props) => props.theme.defaultText};
-      font-size: 1.2em;
+      font-size: 20px;
       font-weight: 100;
+      line-height: 110%;
       margin: 1em 0;
     }
     margin-bottom: 2em;
@@ -170,36 +183,49 @@ const SectionTwo = styled.section`
 const SectionThree = styled.section`
   height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
   min-width: 1200px;
+
   .introduce {
     position: relative;
     width: 60%;
+    text-align: center;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    text-align: center;
-    h1 {
-      font-size: 4em;
-      font-weight: 900;
-
-      line-height: 120%;
+    align-items: center;
+    .contents {
+      h1 {
+        font-size: 65px;
+        font-weight: 800;
+        letter-spacing: -2px;
+        line-height: 110%;
+      }
+      p {
+        font-family: 'Noto Sans KR';
+        font-weight: 100;
+        font-size: 20px;
+        line-height: 110%;
+      }
     }
-    p {
-      font-family: 'Noto Sans KR Light';
-      font-size: 1.5em;
-    }
-
     img {
       position: absolute;
-      left: 20%;
-      width: 60%;
+      top: 50;
+      width: 400px;
       background-color: ${(props) => props.theme.sub};
       z-index: -1;
       opacity: 0.6;
     }
+    .circle {
+      img {
+        width: 400px;
+        background-color: ${(props) => props.theme.sub};
+        // z-index: -1;
+        opacity: 0.6;
+      }
+    }
   }
+
   .image {
     width: 40%;
     display: flex;
