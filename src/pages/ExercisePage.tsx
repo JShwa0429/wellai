3aaaaef0720285b9352bfadea8d9a8f2be5e4511
HongJoonHyung <sesa0802@naver.com>
2022-03-07 20:48:34 +0900

    feat : 코스 운동 페이지 API 연동

--- src/pages/ExercisePage.tsx
+++ src/pages/ExercisePage.tsx
@@ -1,7 +1,10 @@
 import { WebCam, Video, Description } from 'components';
 import styled from 'styled-components';
 import { IoIosExit } from 'react-icons/io';
-import { Link, useParams } from 'react-router-dom';
+import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { CourseApi } from 'api/CourseApi';
+import { exercise } from 'api/common';
 const ExcercisePage = () => {
   // const opts = {
   //   playerVars: {
@@ -9,8 +12,38 @@ const ExcercisePage = () => {
   //     autoplay: 1,
   //   },
   // };
-  const url = '3V3oA8scvyQ';
+
   const { id } = useParams();
+  const [exercises, setExercises] = useState<string[]>([]);
+  const [exerciseNumber, setExerciseNumber] = useState<number>();
+  const [exerciseData, setExerciseData] = useState<exercise>();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const course = CourseApi();
+    course
+      .getDetailInformation(id as string)
+      .then((res) => {
+        setExercises(res.data.exercises);
+        setExerciseNumber(0);
+      })
+      .catch((err) => console.log(err.response));
+  }, []);
+
+  useEffect(() => {
+    const course = CourseApi();
+    course
+      .getExercise(exercises[exerciseNumber as number])
+      .then((res) => setExerciseData(res.data))
+      .catch((err) => console.log(err.response));
+  }, [exerciseNumber]);
+
+  const handleNextExercise = () => {
+    if (exerciseNumber === exercises.length - 1) {
+      navigate(`../course/${id}`);
+    } else {
+      setExerciseNumber((current) => (current as number) + 1);
+    }
+  };
   return (
     <DivCourse>
       <div style={{ width: '100%' }}>
@@ -20,12 +53,13 @@ const ExcercisePage = () => {
             강의실 나가기
           </Button>
         </Link>
+        <button onClick={handleNextExercise}>다음 운동으로</button>
       </div>
       <div style={{ width: '80%' }}>
         <Description />
       </div>
       <VideoDiv>
-        <Video url={url} />
+        <Video url={exerciseData?.youtube_key as string} />
         <WebCam />
       </VideoDiv>
     </DivCourse>
